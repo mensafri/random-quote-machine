1d8b3848208f896925d051e3623151ae6172b171
mensafri <safrijawad1245@gmail.com>
2023-04-12 16:31:23 +0700

    finish

--- src/App.js
+++ src/App.js
@@ -1,58 +1,90 @@
 import { Card, Button } from "react-bootstrap";
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class App extends Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
 
     this.state = {
-      data: {}
-    }
+      quote: "",
+      author: "",
+    };
   }
 
   componentDidMount() {
     let options = {
-      method: 'GET',
-      headers: { 'x-api-key': 'XxnEuRBV/oxIKTKSdII2Zg==JDIxvDsQUIEdZ8CY' }
-    }
-    
-    let url = 'https://api.api-ninjas.com/v1/quotes?category=humor'
+      method: "GET",
+      headers: { "x-api-key": "XxnEuRBV/oxIKTKSdII2Zg==JDIxvDsQUIEdZ8CY" },
+    };
+
+    let url = "https://api.api-ninjas.com/v1/quotes?category=humor";
 
     fetch(url, options)
-      .then((response) => response.json())
-      .then((json) => {
+      .then((res) => res.json()) // parse response as JSON
+      .then((response) => {
         this.setState({
-            data: json
-        })
-        console.log(json);
+          quote: response[0].quote,
+          author: response[0].author,
+        });
+      })
+      .catch((err) => {
+        console.log(`error ${err}`);
       });
   }
 
+  changeQuote = () => {
+    let options = {
+      method: "GET",
+      headers: { "x-api-key": "XxnEuRBV/oxIKTKSdII2Zg==JDIxvDsQUIEdZ8CY" },
+    };
+
+    let url = "https://api.api-ninjas.com/v1/quotes?category=humor";
+
+    fetch(url, options)
+      .then((res) => res.json()) // parse response as JSON
+      .then((response) => {
+        this.setState({
+          quote: response[0].quote,
+          author: response[0].author,
+        });
+      })
+      .catch((err) => {
+        console.log(`error ${err}`);
+      });
+  };
+
   render() {
     return (
-      <div
-      className="bg-dark"
-      id="quote-box"
-      style={{
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        height: "100vh"
-      }}
-    >
-      <Card style={{ width: "18rem" }} border="primary">
-        <Card.Body>
-          <Card.Title id="text">"Quote"</Card.Title>
-          <Card.Text id="author">author</Card.Text>
-          <Button variant="primary" id="tweet-quote" target="_blank">
-            Tweet
-          </Button>
-          <Button variant="primary" id="new-quote">
-            New Quote
-          </Button>
-        </Card.Body>
-      </Card>
-    </div>
-    )
+      <div className="bg-dark container-fluid d-flex justify-content-center align-items-center" style={{
+        overflow: "hidden",
+        height: "100vh",
+      }}>
+        <div className="row text-center d-flex align-items-center" id="quote-box">
+          <Card style={{ width: "25rem" }}>
+            <Card.Body>
+              <Card.Title id="text">"{this.state.quote}"</Card.Title>
+              <Card.Text id="author">{this.state.author}</Card.Text>
+              <Button
+                variant="secondary"
+                id="new-quote"
+                className="mr-5"
+                onClick={this.changeQuote}
+              >
+                New Quote
+              </Button>
+              <a
+                variant="primary"
+                id="tweet-quote"
+                className="ml-5"
+                href="twitter.com/intent/tweet"
+                target="_top"
+              >
+                Tweet
+              </a>
+            </Card.Body>
+          </Card>
+        </div>
+      </div>
+    );
   }
 }
