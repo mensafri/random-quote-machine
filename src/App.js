2374e103148ad78f2f7bbe1e1482842452aabac7
mensafri <safrijawad1245@gmail.com>
2023-04-12 09:41:46 +0700

    quote api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,58 @@
+import { Card, Button } from "react-bootstrap";
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <h2>Hallo</h2>
+export default class App extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      data: {}
+    }
+  }
+
+  componentDidMount() {
+    let options = {
+      method: 'GET',
+      headers: { 'x-api-key': 'XxnEuRBV/oxIKTKSdII2Zg==JDIxvDsQUIEdZ8CY' }
+    }
+    
+    let url = 'https://api.api-ninjas.com/v1/quotes?category=humor'
+
+    fetch(url, options)
+      .then((response) => response.json())
+      .then((json) => {
+        this.setState({
+            data: json
+        })
+        console.log(json);
+      });
+  }
+
+  render() {
+    return (
+      <div
+      className="bg-dark"
+      id="quote-box"
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh"
+      }}
+    >
+      <Card style={{ width: "18rem" }} border="primary">
+        <Card.Body>
+          <Card.Title id="text">"Quote"</Card.Title>
+          <Card.Text id="author">author</Card.Text>
+          <Button variant="primary" id="tweet-quote" target="_blank">
+            Tweet
+          </Button>
+          <Button variant="primary" id="new-quote">
+            New Quote
+          </Button>
+        </Card.Body>
+      </Card>
     </div>
-  );
+    )
+  }
 }
-
-export default App;
